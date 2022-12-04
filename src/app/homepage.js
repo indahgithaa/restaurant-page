e5e56f20b647cfb3bf39cfd784e98e70ed2500a6
indahgitha <indahgitha99@gmail.com>
2022-12-05 04:44:50 +0800

    Update homepage

--- src/app/homepage.js
+++ src/app/homepage.js
@@ -1,9 +1,28 @@
+import cover from '../assets/cover.png'
+
 export default function homePage() {
     const content = document.querySelector('#content');
 
+    const homepage = document.createElement('div')
     const coverImg = document.createElement('img');
-    coverImg.src = '../assets/cover.png';
-    coverImg.setAttribute('id', 'coverImg')
+    const welcomingWrap = document.createElement('div');
+    const welcoming = document.createElement('h1');
+    const exploreBtn = document.createElement('button');
 
-    content.appendChild(coverImg)
+    homepage.setAttribute('class', 'homepage');
+    coverImg.setAttribute('src', cover)
+    coverImg.setAttribute('id', 'coverImg');
+    exploreBtn.setAttribute('id', 'expBtn');
+    welcomingWrap.setAttribute('id', 'welcomingWrap');
+    welcoming.setAttribute('id', 'welcoming');
+
+    welcoming.innerHTML = 'The Goodness of <span> Real Foods </span> for Your Healthier Life';
+    exploreBtn.textContent = 'Explore Our Menu'
+
+    welcomingWrap.appendChild(welcoming);
+    welcoming.appendChild(exploreBtn);
+    homepage.appendChild(coverImg);
+    homepage.appendChild(welcomingWrap);
+
+    content.appendChild(homepage);
 }
